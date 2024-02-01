fd0b53ab851571867a0f934e4c595e5b520f80a5
Raimondas <raimondas.sileikis@gmai.com>
2024-02-01 13:04:08 +0200

    react login signup

--- react/src/views/public/SignUp.jsx
+++ react/src/views/public/SignUp.jsx
@@ -1,7 +1,150 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import axiosClient from '../../axios';
 
-export default function SignUp() {
+export default function Signup() {
+  // const { setCurrentUser, setUserToken } = UseStateContext();
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [error, setError] = useState({ __html: '' });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setError({ __html: '' });
+    console.log('signup');
+    axiosClient.post('/signup', {
+      name: fullName,
+      email,
+      password,
+      password_confirmation: passwordConfirmation,
+    })
+      .then(({ data }) => {
+        // setCurrentUser(data.user);
+        // setUserToken(data.token);
+      })
+      .catch((error) => {
+        if (error.response) {
+          const errors = Object.values(error.response.data.errors).reduce((acc, next) => [...acc, ...next], [])
+          console.log(errors);
+          setError({ __html: errors.join('<br>') })
+        }
+        console.log(error);
+      })
+  }
+
   return (
-    <div>SignUp</div>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          SignUp
+        </h2>
+      </div>
+      {error.__html && (
+        <div
+          className='bg-red-500 rounded py-2 px-3 text-white'
+          dangerouslySetInnerHTML={error}>
+        </div>
+      )}
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={onSubmit} className="space-y-6" action="#" method="POST">
+          <div>
+            <label htmlFor="full-name" className="block text-sm font-medium leading-6 text-gray-900">
+              Full Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="full-name"
+                name="name"
+                type="text"
+                required
+                value={fullName}
+                onChange={e => setFullName(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Full name"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Email address"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              Password Confirmation
+              <label htmlFor="password-confirmation" className="block text-sm font-medium leading-6 text-gray-900">
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password-confirmation"
+                name="password_confirmation"
+                type="password"
+                required
+                value={passwordConfirmation}
+                onChange={e => setPasswordConfirmation(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Password Confirmation"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              SignUp
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Do your have account?{' '}
+          <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            Login with your account
+          </Link>
+        </p>
+      </div>
+    </div>
+
   )
 }
